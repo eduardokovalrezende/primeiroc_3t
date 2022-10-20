65d7404aea3366a0739c1aeb9cc98f6002d4b99c
eduardokovalrezende <110104869+eduardokovalrezende@users.noreply.github.com>
2022-10-20 12:50:25 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,50 +1,64 @@
-let tabuada = 1;
+let tabuada = 5;
 
 function escreva(){
-    document.write(tabuada + " x 1 =  " + (tabuada*1) + "<br>");
-    document.write(tabuada + " x 2 =  " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 =  " + (tabuada*3) + "<br>");
-    document.write(tabuada + " x 4 =  " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 =  " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 =  " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 =  " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 =  " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 =  " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 =  " + (tabuada*10) + "<br>");
+    document.write("Tabuada do " + tabuada + "<br>")
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");   
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
- function minhaTabuada(){
-     for(let i = 1; i <= 100; i++){
-         document.write("Tabuada do " + i + "<br>");
-         for(let j = 1; j <= 10; j++){
-            document.write(i + " x " + j + " = " + (j*i) + "<br>");
-         }
-         document.write("<br>");
-     }
- }
+let lista = ["Jesus","Spessato","Jorge","Raquel"]
 
- function cubo(){
-     for(let i = 20; i >= 2; i--){
-         document.write("O cubo de " + i + " é " + i + " x " + i + " x " + i + " = " + (i*i*i)+"<br>")
-     }
- }
- function mat(){
-     let v = document.getElementById("valor").value;
-     let j = document.getElementById("juros").value;
-     let t = document.getElementById("meses").value;
-     let r = 0;
-     for(let i = 1; i<=t; i++){
-         r = v * (1+ (j/100));
-         v = r;
-     }
-     let r = v * (1=(j/100)
-     document.write("Resultado " + r);
- }
+function mostra(){
+    document.write(lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write(lista[i] + "<br>");
+    }  
+}
+
+function mostratabuada(){
+    for(let i = 1; i <= 10; i++){
+        document.write("O valor do i " + i + "<br>");
+    }
+}
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0; 
+    for(let i =1; i <=t; i++){
+        r = v * (1 + (j/100));
+        document.write("Mẽs " + i + " - valor: " + r + "<br>");
+        v = r;
+    }
+    document.write("Resultado: " + r);
+}
+function SomaNota(){
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+
+    let r =  Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+    document.getElementById("resul").innerHTML = "Soma: " + r;
+
+}
 function media(){
-let n1 = document.getElementById("v1").value;
-let n2 = document.getElementById("v2").value;
-let n3 = document.getElementById("v3").value;
-let r = (Number(n1) +  Number(n2) Number(n3));
-document.getElementById("resultado").innerHTML = "media: " + r:
+    let n1 = document.getElementById("b1").value;
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3").value;
+    let n4 = document.getElementById("b4").value;
+    
+    let r =  Number(n1)+Number(n2)+Number(n3)+Number(n4);
 
+    document.getElementById("resul").innerHTML = "Resultado " + r /4;
+   
 }
